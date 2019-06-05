669e90b2bf1687f776bed194765d6b13eca073af
Emily Miller-McGlone <emilymiller-mcglone@Emilys-MacBook-Pro.local>
2019-06-04 20:37:59 -0400

    cleanup

--- CantinaProgrammingExercise.py
+++ CantinaProgrammingExercise.py
@@ -6,33 +6,25 @@ with open("SystemViewController.json", "r") as JSONFile:
 data = json.loads(exerciseData)
 
 
-def matchSelector(attribute, selector, d):
+def matchingViews(attribute, selector, d):
+    """ Find matching views based on the selector """
 
     class NameSpace(object):
         pass
     ns = NameSpace()
-    ns.count = 0
     ns.matches = []
 
     def inner(attribute, selector, d):
         for key, value in d.items():
-            # if value is a list
             if isinstance(value, list):
-                # recurse through list items
                 for item in value:
-                    # if list items are a dictionary, recurse
                     if isinstance(item, dict):
                         inner(attribute, selector, item)
-                    # if item is not a dictionary
                     elif key == attribute and item == selector:
-                        ns.count += 1
                         ns.matches.append(d)
-            # if value is a dict
             elif isinstance(value, dict):
                 inner(attribute, selector, value)
-            # check if key/value matches the attribute and selector
             elif key == attribute and value == selector:
-                ns.count += 1
                 ns.matches.append(d)
 
     inner(attribute, selector, d)
@@ -40,22 +32,22 @@ def matchSelector(attribute, selector, d):
 
 
 def parseSelector(input):
-    """ Determine the view attribute """
+    """ Determine the view attribute from the input """
     if input[0] == ".":
-        result = matchSelector("classNames", input[1:], data)
+        result = matchingViews("classNames", input[1:], data)
     elif input[0] == "#":
-        result = matchSelector("identifier", input[1:], data)
+        result = matchingViews("identifier", input[1:], data)
     else:
-        result = matchSelector("class", input, data)
+        result = matchingViews("class", input, data)
     return result
 
 
 def echoInput(input, output):
+    "Echo input to command line"
     result = json.dumps(parseSelector(input))
     output.write(result)
 
 
-#matchSelector("classNames", "columns", data)
 if __name__ == "__main__":
     while True:
         echoInput(input("\nEnter a selector: "), sys.stdout)
